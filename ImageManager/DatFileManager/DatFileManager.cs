b48aa568f23c26883f3ef5db23b74b7a893fd93c
AonaSuzutsuki <rvvfake.org@gmail.com>
2018-06-07 17:19:15 +0900

    add datfilemanager.

---
+++ ImageManager/DatFileManager/DatFileManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace DatFileManager
+{
+	public class DatFileManager : IDisposable
+    {
+        
+		private const int LEN = 4;
+
+		#region Fields
+		private string filePath;
+		private FileStream fileStream;
+		#endregion
+
+		public DatFileManager(string filePath)
+        {
+			this.filePath = filePath;
+			Open();
+        }
+
+		public byte[] GetBytes(int start)
+		{
+			byte[] data = null;
+
+			if (fileStream != null)
+			{
+				var lenArray = new byte[LEN];
+				fileStream.Read(lenArray, start, lenArray.Length);
+				var length = BitConverter.ToInt32(lenArray, 0);
+
+				data = new byte[length];
+				fileStream.Read(data, start + 4, data.Length);
+			}
+
+			return data;
+		}
+
+		public void Dispose()
+        {
+            ((IDisposable)fileStream).Dispose();
+        }
+
+
+		private void Open()
+		{
+			fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+		}
+    }
+}
