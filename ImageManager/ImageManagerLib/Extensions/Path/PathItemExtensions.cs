f0e75075cfd17ba2bbb8709cd3a4bb42d1924746
AonaSuzutsuki <rvvfake.org@gmail.com>
2018-06-22 17:25:06 +0900

    Rename files.

--- ImageManager/ImageManagerLib/Extensions/Path/PathItemExtensions.cs
+++ ImageManager/ImageManagerLib/Extensions/Path/PathItemExtensions.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileManagerLib.Extentions.Path
+namespace FileManagerLib.Extensions.Path
 {
     public static class PathItemExtensions
     {
