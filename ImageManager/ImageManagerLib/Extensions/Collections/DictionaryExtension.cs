2c12c81ba8fcacea2e4b802a316570c506fdcdd8
AonaSuzutsuki <rvvfake.org@gmail.com>
2018-06-22 17:26:00 +0900

    add extension.

---
+++ ImageManager/ImageManagerLib/Extensions/Collections/DictionaryExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileManagerLib.Extensions.Collections
+{
+	public static class DictionaryExtension
+    {
+		public static (TKey, TValue) GetLast<TKey, TValue>(this Dictionary<TKey, TValue> dic)
+		{
+			var keys = new List<TKey>(dic.Keys);
+			var values = new List<TValue>(dic.Values);
+			var key = keys[keys.Count - 1];
+			var val = values[values.Count - 1];
+			return (key, val);
+		}
+    }
+}
