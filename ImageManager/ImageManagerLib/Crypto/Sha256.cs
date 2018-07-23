6049c7cb07103b546448c95934f196ebbb72d594
Aona Suzutsuki <aonasuzutsuki@gmail.com>
2018-07-23 23:48:24 +0900

    Changed hash buffer size.

--- ImageManager/ImageManagerLib/Crypto/Sha256.cs
+++ ImageManager/ImageManagerLib/Crypto/Sha256.cs
@@ -12,7 +12,7 @@ namespace FileManagerLib.Crypto
 
 		public static string GetSha256(Stream stream, Action<Stream> disposeAction = null)
 		{
-			var bufferedStream = new BufferedStream(stream, 1024 * 32);
+			var bufferedStream = new BufferedStream(stream, 10485760);
             var sha = new SHA256Managed();
 			byte[] checksum = sha.ComputeHash(bufferedStream);
             stream.Seek(0, SeekOrigin.Begin);
