2a59aa06f70137eee50b683d5543e409389f6b84
Aona Suzutsuki <aonasuzutsuki@gmail.com>
2018-07-27 01:32:17 +0900

    add test gui

---
+++ ImageManager/ImageManager/Models/MainWindowModel.cs
@@ -0,0 +1,30 @@
+﻿using CommonStyleLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageManager.Models
+{
+    public class MainWindowModel : ModelBase
+    {
+        #region Fields
+        private string testText;
+        #endregion
+
+        #region Properties
+        public string TestText
+        {
+            get => testText;
+            set => SetProperty(ref testText, value);
+        }
+        #endregion
+
+
+        public void TestClicked()
+        {
+            TestText = "Test";
+        }
+    }
+}
---
+++ ImageManager/ImageManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using CommonStyleLib.Models;
+using CommonStyleLib.ViewModels;
+using ImageManager.Models;
+using Prism.Commands;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ImageManager.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        #region Fields
+        private readonly MainWindowModel model;
+        #endregion
+
+        public MainWindowViewModel(Window view, MainWindowModel model) : base(view, model)
+        {
+
+            this.model = model;
+
+            #region Initialize Events
+            TestClicked = new DelegateCommand(Test_Clicked);
+            #endregion
+
+            #region Initialize Properties
+            TestText = model.ToReactivePropertyAsSynchronized(m => m.TestText);
+            #endregion
+
+            DoLoaded();
+        }
+
+        #region Properties
+        public ReactiveProperty<string> TestText { get; set; }
+        #endregion
+
+        #region Event Properties
+        public ICommand TestClicked { get; set; }
+        #endregion
+
+        #region Event Methods
+        private void Test_Clicked()
+        {
+            model.TestClicked();
+        }
+        #endregion
+    }
+}
